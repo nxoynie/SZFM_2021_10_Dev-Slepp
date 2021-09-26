f55f84bfa3b506dc4472093e06703d8164e3efb0
ud-flori <flori.zs14@gmail.com>
2021-09-26 15:48:14 +0200

    CODE - Calculator Backend.

---
+++ calculator/src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,25 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CalculatorApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(CalculatorApplication.class.getResource("main.fxml"));
+        stage.setTitle("Calculator Name");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+        root.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ calculator/src/main/java/com/example/calculator/CalculatorController.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CalculatorController {
+    @FXML
+    private Label welcomeText;
+
+}
